965bf52043239d91835a5209e513cede8ae99f38
Pranav Pise <itscap1810@gmail.com>
2024-06-16 10:19:38 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
@@ -8,13 +10,28 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.get('/hello', function(req, res){
+  res.json({greetins: "Hello, API"});
 });
 
 
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log('Node is listening on port ' + listener.address().port)
+  });
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+var multer = require('multer')
+
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response) => {
+  let responseObject = {}
+  responseObject['name'] = request.file.originalname
+  responseObject['type'] = request.file.mimetype
+  responseObject['size'] = request.file.size
+  
+  response.json(responseObject)
+} )
+
+
